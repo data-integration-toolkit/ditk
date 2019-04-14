90e1647f173aa4755c27720a1657eee3d6dc2dc2
raschild <stripeli@usc.edu>
2019-04-13 22:06:47 -0700

    Pushing Imputation Tests

--- data_cleaning/imputation/testexample/sample_test.py
+++ data_cleaning/imputation/testexample/sample_test.py
@@ -10,50 +10,46 @@ class TestImputationMethods(unittest.TestCase):
 	def setUp(self):
 		self.imputation_method = "impute_method"  # The implementation of your Imputation method
 		self.input_file = "path to the input filename"
-		self.verificationErrors = []
+		self.verificationErrors = [] # append exceptions for try-except errors
 
 	def test_input_file_format(self):
 		# test if input file agrees with expected format
-		try:
+		with open(self.input_file, "r") as fin:
+			lines = csv.reader(fin)
+			total_lines = 0
+			for line in lines:
+				total_lines += 1
+
+	def test_impute(self):
+
+		# Test whether the final imputed data have the same shape with input data
+		with open(self.input_file, "r") as fin:
+			lines = csv.reader(fin)
+			input_headers = next(lines)
+			total_input_lines = 0
+			for line in lines:
+				total_input_lines += 1
+
+		preprocess_result = self.imputation_method.preprocess(self.input_file)
+		if isinstance(preprocess_result, np.ndarray):
+			self.assertEquals(preprocess_result.shape[0], total_input_lines)
+			self.assertEquals(preprocess_result.shape[1], input_headers)
+		elif isinstance(preprocess_result, pd.DataFrame):
+			self.assertEquals(preprocess_result.shape[0], total_input_lines)
+			self.assertEquals(preprocess_result.shape[1], input_headers)
+		else:
 			with open(self.input_file, "r") as fin:
 				lines = csv.reader(fin)
-				total_lines = 0
+				output_headers = next(lines)
+				total_output_lines = 0
 				for line in lines:
-					total_lines += 1
-		except Exception as e:
-			self.verificationErrors.append(e)
-
-	def test_preprocess(self):
-		preprocess_result = self.imputation_method.preprocess(self.input_file)
-
-		try: self.assertIsInstance(preprocess_result, pd.DataFrame) # Check if type is Data Frame
-		except AssertionError as e: self.verificationErrors.append(e)
-		try: self.assertIsInstance(preprocess_result, np.ndarray) # Check if type is Numpy
-		except AssertionError as e: self.verificationErrors.append(e)
-		try:
-			with open(preprocess_result, "r") as fin:
-				lines = csv.reader(fin)
-				total_lines = 0
-				for line in lines:
-					total_lines = 0
-				self.verificationErrors.append("Non Empty ")
+					total_output_lines += 1
+			self.assertEquals(total_input_lines, total_output_lines)
+			self.assertEquals(input_headers, output_headers)
 
 
-
-	def test_predict_format(self):
-		prediction_score = tss.predict(data_X, data_Y)
-		assertTrue(isFloat(prediction_score))
-		assertTrue(prediction_score >= 0 and prediction_score <= 1.0)
-		with self.assertRaises(ValueError):
-			tss.predict(empty_list)  # Fails on wrong input format
-
-	def test_predict_similar(self):
-		prediction_score = tss.predict(data_X, data_X)
-		assertTrue(prediction_score == 1.0)  # Same sentences
-
-	def test_predict_dissimilar(self):
-		prediction_score = tss.predict(data_X, data_Y)
-		assertTrue(prediction_score < 1.0)  # Dissimilar sentences
+	def test_evaluate(self, *args, **kwargs):
+		self.assertIsInstance(self.imputation_method.evaluate(*args, **kwargs), float)
 
 
 if __name__ == '__main__':
