862ef810231db3ae9659773dc0e7e4389cc8e33a
Suhrid Karthik <suhridk@gmail.com>
2019-04-23 18:50:58 -0700

    Sample input/output for graph completion, added a new test

--- graph/completion/testexample/sample_test.py
+++ graph/completion/testexample/sample_test.py
@@ -6,8 +6,9 @@ from graph.completion.graph_completion import graph_completion
 class TestGraphCompletionMethods(unittest.TestCase):
 
 	def setUp(self, input_file):
-		self.graph_completion = GraphCompletion() # initialize your Blocking method
+		self.graph_completion = GraphCompletion() # initialize your specific Graph Completion class
 		self.input_file = input_file
+        self.output_file = graph_completion.main(input_file)
 
 	def test_read_dataset(self):
 		train, test, dev = self.graph_completion.read_dataset()
@@ -29,6 +30,20 @@ class TestGraphCompletionMethods(unittest.TestCase):
 		self.assertIsInstance(evaluations, dict)
 		self.assertIn("f1", evaluations)
 		self.assertIn("MRR", evaluations)
+
+	def test_output_facts():
+		data = pd.read_csv(input_file,delim=' ')
+		input_entity1 = data.Entity1.unique().tolist()
+		input_entity2 = data.Entity1.unique().tolist()
+		input_relations = data.Relation.unique().tolist()
+		input_entities = input_entity1 + input_entity2
+		data = pd.read_csv(output_file,delim=' ')
+		output_entity1 = data.Entity1.unique().tolist()
+		output_entity2 = data.Entity1.unique().tolist()
+		output_entities = output_entity1 + output_entity2
+		output_relations = data.Relation.unique().tolist()
+		self.assertEqual(set(input_entities),set(output_entities))
+		self.assertEqual(set(input_relations),set(output_relations))
 
 if __name__ == '__main__':
     unittest.main()
