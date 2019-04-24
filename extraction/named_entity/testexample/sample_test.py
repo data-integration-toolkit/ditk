ae03d4f4e4e11df75a0bcfdb83531191795a6dea
Joel Mathew <joelmathew.mec@gmail.com>
2019-04-24 16:51:58 -0700

    Update test example.

--- extraction/named_entity/testexample/sample_test.py
+++ extraction/named_entity/testexample/sample_test.py
@@ -13,14 +13,14 @@ class TestNERMethods(unittest.TestCase):
         df = pd.read_csv(file_name,delim=' ')
         return df.shape
 
-    def test_outputformat(self):    
+    def test_outputformat(self):
         input_row_count = row_col_count(input_file)[0]
         input_col_count = row_col_count(input_file)[1]
         output_row_count = row_col_count(output_file)[0]
         output_col_count = row_col_count(output_file)[1]
 
-        assertEqual(input_row_count, output_row_count)
-        assertEqual(input_col_count + 1, output_col_count)
+        self.assertEqual(input_row_count, output_row_count)
+        self.assertEqual(output_col_count, 3)
 
 if __name__ == '__main__':
     unittest.main()
