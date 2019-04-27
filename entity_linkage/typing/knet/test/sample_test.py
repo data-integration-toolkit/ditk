7c5da55e7439a22b12d15320ecc1e0d3fb6813dd
Gyanesh Mittal <gyanesh.cse@gmail.com>
2019-04-27 10:10:44 -0700

    updated the unittest

--- entity_linkage/typing/knet/test/sample_test.py
+++ entity_linkage/typing/knet/test/sample_test.py
@@ -1,23 +1,25 @@
 import unittest
 import pandas as pd
-from typing import entity_typing 
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from main import main
 
-def row_col_count(file_name):
-    df = pd.read_csv(file_name, delimiter='\t')
-    return df.shape
 
 class TestEntityTypingMethods(unittest.TestCase):
-
     def setUp(self):
-        self.et = entity_typing() #Your implementation of Entity Typing
         self.input_file = 'entity_typing_test_input.txt'
-        self.output_file = et.main(input_file)
+        self.output_file = main(self.input_file)
 
-    def test_outputformat(self):    
-        input_row_count = row_col_count(self.input_file)[0]
-        input_col_count = row_col_count(self.input_file)[1]
-        output_row_count = row_col_count(self.output_file)[0]
-        output_col_count = row_col_count(self.output_file)[1]
+    def row_col_count(self, file_name):
+        df = pd.read_csv(file_name, delimiter='\t')
+        return df.shape
+
+    def test_outputformat(self):
+        input_row_count = self.row_col_count(self.input_file)[0]
+        input_col_count = self.row_col_count(self.input_file)[1]
+        output_row_count = self.row_col_count(self.output_file)[0]
+        output_col_count = self.row_col_count(self.output_file)[1]
 
         self.assertEqual(input_row_count, output_row_count)
         self.assertEqual(input_col_count, output_col_count + 1)
