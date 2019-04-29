f6655e9018b99c6929876ed63469a98bc8c8dd2d
trishav96 <trishav96@gmail.com>
2019-04-29 15:33:36 -0700

    Update __init__.py

--- extraction/named_entity/gcn-ner/gcn_ner/utils/training/__init__.py
+++ extraction/named_entity/gcn-ner/gcn_ner/utils/training/__init__.py
@@ -46,29 +46,17 @@ def train_and_save(dataset, saving_dir, epochs=20, bucket_size=10):
             try:
                 gcn_bucket = []
                 for item in bucket:
-                    #print("a")
                     words = item[0]
-                    #print("b")
                     word_embeddings = item[1]
-                    #print("c")
                     sentence = create_full_sentence(words)
-                    #print("d")
                     tags = item[2]
-                    #print("e")
                     label = item[3]
-                    #print("f")
                     label = [np.array(l, dtype=np.float32) for l in label]
-                    #print("g")
                     A_fw, A_bw, _, X = aux.create_graph_from_sentence_and_word_vectors(sentence, word_embeddings)
-                    #print("h")
                     gcn_bucket.append((A_fw, A_bw, X, tags, label))
-                    #print("i")
                 if len(gcn_bucket) > 1:
-                    #print("j")
                     gcn_model.train(gcn_bucket, trans_prob, 1)
-                    #print("k")
             except:
-               #print("l")
                pass
         save_filename = saving_dir + 'ner-gcn-' + str(i) + '.tf'
     return (save_filename, gcn_model)
