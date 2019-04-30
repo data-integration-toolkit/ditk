58453e3050d3e3b548f2390432f9aae220efdde6
sandiexie-USC <zhonghux@usc.edu>
2019-04-30 11:52:01 -0700

    create TransE

---
+++ graph/embedding/TransE/Readme.md
@@ -0,0 +1,82 @@
+# TransE
+
+- Translating Embeddings for Modeling Multi-relational Data
+- Bordes A., Usunier N., Garcia-Dur´an A., Weston J., and Yakhnenko O.(2013) Translating embeddings for modeling multi-relational data. In Proceedings of NIPS, 2013.
+
+## Original Code
+
+A general TransE implementation:
+
+- https://github.com/thunlp/KB2E
+
+A Python and Tensorflow implementation used in this model:
+
+- https://github.com/ZichaoHuang/TransE
+
+## Description
+
+- TransE embed entities and relationships of metarelational data in low-dimensional vector spaces. TransE models relationships by interpreting them as translations operating on the low-dimensional embeddings of the entities. The task of this method includes embedding the training data, and using the embeddings to predict head/tail entities. 
+
+- ![1556516303912](C:\Users\Sandie\AppData\Roaming\Typora\typora-user-images\1556516303912.png)
+
+  *This image shows the idea of interpreting relation(r) as transitions from head entity(h) to tail entity(t).
+
+- The TransE model provided in this repository contains functions to read a training file as input, to learn embeddings of the inputs, and to preform evaluations on the embeddings. 
+
+## Input and Output
+
+Input for embedding:
+
+​	Files:
+
+   - train.txt (in the format of <head_entity, tail_entity, relation>)
+
+   - test.txt (can be split from train.txt)
+
+   - valid.txt (can be split from train.txt)
+
+   - entity2id.txt (<entity_name, eneity_id>)
+
+   - relation2id.txt (<relation_name, relation_id>)
+
+     *entity2id.txt  and relation2id.txt can be generated from train.txt by assigning each entity and relationship a unique id.
+
+​	Parameters:
+
+- input_folder : containing all the input files
+- dimension: dimension to embed each entity and relationship to
+- marginal_value: goal value to stop iteration
+- batch_size: batch size for gradient descent
+- max_epoch: max number of iterations
+
+
+
+Output:
+
+Files:
+
+- embedded_entity.txt (in format <entity_name: embedding>)
+- embedded_relation.txt(in format <relation_name: embedding>)
+
+## Evaluation
+
+- Benchmark datasets:
+
+  - WN18 - WordNet dataset in format of <head_entity, tail_entity, relation>
+
+  - FB15k - FreeBase dataset of size 15k in format of <head_entity, tail_entity, relation>
+
+  - YAGO - original dataset in format of <head_id, relation_id, tail_id>, cleaned to format <head_entity, tail_entity, relation> for this model to use.
+
+    |       | epoch | MeanRank | Hits@10 |
+    | ----- | ----- | -------- | ------- |
+    | WN18  | 1000  | 243      | 79.9    |
+    | FB15k | 1000  | 546      | 36.8    |
+    | YAGO  | 1000  | 5786     | 0.001   |
+
+    
+
+## Demo
+
+- Link to the Jupyter Notebook
+- Link to the video on Youtube
---
+++ graph/embedding/TransE/tests/test_transE.py
@@ -0,0 +1,86 @@
+# This is a main file that evoke entire codes
+
+import sys
+import os
+#dir_path = os.path.dirname(os.path.realpath(__file__))
+#sys.path.insert(0, dir_path + "/src")
+
+#from main import main
+#dir_path = os.path.dirname(os.path.realpath(__file__))
+#print (dir_path)
+import unittest
+import tensorflow as tf
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(os.path.abspath(".."), ""))
+os.chdir("..")
+
+from graph_embedding_transE import MyTransE
+
+class TestGraphEmbeddingMethods(unittest.TestCase):
+    def setUp(self):
+            #self.graph_embedding = GraphEmbedding() # initialize your Blocking method
+            self.graph_embedding = MyTransE()
+            self.input_file = './data/WN18/'
+            self.dimension = 300
+            self.marginal_value = 1.0
+            self.batch_size = 4800
+            self.max_epoch = 3
+
+    def read_dataset(self):
+            train, validation, test = self.graph_embedding.read_dataset(self.input_file)
+            # If possible check if the read_dataset() function returns data of similar format (e.g. vectors of any size, lists of lists, etc..)
+            self.assertTrue(train, list) # assert non-empty list
+            self.assertTrue(validation, list) # assert non-empty list
+            self.assertTrue(test, list) # assert non-empty list
+
+    def test_learn_embeddings(self):
+            self.graph_embedding.read_dataset(self.input_file)
+            # This fucntion could check on whether the embeddings were generated and if yes, then
+            # it can check on whether the file exists
+            embedding_vector, n_entity, n_relation = self.graph_embedding.learn_embeddings(self.dimension, self.marginal_value, self.batch_size, self.max_epoch)
+
+            self.graph_embedding.save_model('./')
+
+            assert os.path.exists("./embedded_entity.out")
+            assert os.path.exists("./embedded_relations.out")
+
+            self.assertEqual(embedding_vector.shape[0], (n_entity + n_relation))
+            self.assertEqual(embedding_vector.shape[1],300) #Example: output vec should be 3 x 300
+
+    def test_evaluate(self):
+            tf.reset_default_graph()
+            self.graph_embedding.read_dataset(self.input_file)
+            self.graph_embedding.learn_embeddings(self.dimension, self.marginal_value, self.batch_size, self.max_epoch)
+            evaluations = self.graph_embedding.evaluate()
+            # Evaluations could be a dictionary or a sequence of metrics names
+
+            self.assertIsInstance(evaluations, dict)
+            self.assertIn("MRR", evaluations)
+            self.assertIn("Hits", evaluations)
+
+            #print('-----Average-----')
+            #print('MeanRank: {:.3f}, Hits@10: {:.3f}'.format(evaluations['MRR'],evaluations['Hits']))
+
+            file = open("Evaluation.txt","w")
+            file.write('-----Average-----')
+            file.write('MeanRank: {:.3f}, Hits@10: {:.3f}'.format(evaluations['MRR'],evaluations['Hits']))
+            file.close()
+
+if __name__ == '__main__':
+    unittest.main()
+    """
+    # initial TransE
+    TransE = TransE()
+
+    # Set the data to use
+    # Select from ["FB15", "WN18", "test case"]
+    TransE.read_dataset("WN18")
+    print(TransE.dataset_name)
+
+    TransE.learn_embeddings()
+    print(TransE.graph_embedded_entity.shape)
+    print(TransE.graph_embedded_relations.shape)
+
+    TransE.save_model('./')
+    """
