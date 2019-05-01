0f4abc5caa6c8fb095aa1b4b4200e86014d2ef4b
kendall <kcrouch@usc.edu>
2019-05-01 12:07:32 -0700

    bug fixes

--- extraction/named_entity/biocppi_extraction/main.py
+++ extraction/named_entity/biocppi_extraction/main.py
@@ -16,9 +16,9 @@ def main(inputFilePath):
     # convert dataset to properformat used by training
     # 1] read_dataset()
 
-    # test unittest...good!
-    # dataset_name = 'unittest'
-    # file_dict = {'train':{'data':inputFilePath},'dev':{},'test':{}}
+    test unittest...good!
+    dataset_name = 'unittest'
+    file_dict = {'train':{'data':inputFilePath},'dev':{},'test':{}}
 
     # # test conll2003...good!..yet to test full dataset
     # dataset_name = 'CoNLL_2003'
@@ -29,14 +29,14 @@ def main(inputFilePath):
     # raw_data_test_file = dataset_dir + 'test.txt'
     # file_dict = {'train':{'data':raw_data_train_file},'dev':{'data':raw_data_dev_file},'test':{'data':raw_data_test_file}}
 
-    # test ontoNotes5.0...good!..yet to test full dataset
-    dataset_name = 'OntoNotes_5p0'
-    dataset_dir = '/home/kcrouch/smol_datasets/ontonotes/'  # smol sample
-    # dataset_dir = '/Users/olderhorselover/USC/spring2019/csci_548_diotw/project/groupedProject/ontoNotes5_corpus/OntoNotes-5.0-NER-BIO/'
-    raw_data_train_file = dataset_dir + 'onto.train.ner'
-    raw_data_dev_file = dataset_dir + 'onto.development.ner'
-    raw_data_test_file = dataset_dir + 'onto.test.ner'
-    file_dict = {'train':{'data':raw_data_train_file},'dev':{'data':raw_data_dev_file},'test':{'data':raw_data_test_file}}
+    # # test ontoNotes5.0...good!..yet to test full dataset
+    # dataset_name = 'OntoNotes_5p0'
+    # dataset_dir = '/home/kcrouch/smol_datasets/ontonotes/'  # smol sample
+    # # dataset_dir = '/Users/olderhorselover/USC/spring2019/csci_548_diotw/project/groupedProject/ontoNotes5_corpus/OntoNotes-5.0-NER-BIO/'
+    # raw_data_train_file = dataset_dir + 'onto.train.ner'
+    # raw_data_dev_file = dataset_dir + 'onto.development.ner'
+    # raw_data_test_file = dataset_dir + 'onto.test.ner'
+    # file_dict = {'train':{'data':raw_data_train_file},'dev':{'data':raw_data_dev_file},'test':{'data':raw_data_test_file}}
 
     # # test CHEMDNER...good!
     # dataset_name = 'CHEMDNER'
