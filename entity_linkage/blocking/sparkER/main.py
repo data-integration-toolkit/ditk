518964c02e18cfb8d2f749e8d0fbd5e05609474e
ikshwaku <sushanths94@gmail.com>
2019-04-30 21:16:04 -0700

    Adding sparkER module

---
+++ entity_linkage/blocking/sparkER/main.py
@@ -0,0 +1,28 @@
+from sparkER import SparkER
+import csv
+
+def main(input_file_path):
+	output_file_path = "output.txt"
+	with open(input_file_path) as f:
+		dataset1 = f.readline().strip()
+		dataset2 = f.readline().strip()
+	sparker = SparkER()
+	#dataset1 = "Dataset1.csv"
+	#dataset2 = "Dataset2.csv"
+	file_list = [dataset1, dataset2]
+	dataframes = sparker.read_dataset(file_list)
+	print(dataframes[0].head(5))
+	sparker.train(dataframes)
+	predicted_pair_list = sparker.predict(dataframe_list = dataframes)
+	print(predicted_pair_list[0][10])
+	groundtruth = "moviesGround.csv"
+	print(sparker.evaluate(groundtruth, dataframes))
+	f = open(output_file_path, "w+b")
+	c = csv.writer(f)
+	c.writerows(predicted_pair_list[0])
+	f.close()
+
+	return output_file_path
+
+if __name__ == '__main__':
+    main("input.txt")
