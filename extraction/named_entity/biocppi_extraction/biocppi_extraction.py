6ca8e0e49f63f407a445fc72133c4dc1d6b4b07b
kendall <kcrouch@usc.edu>
2019-05-01 11:12:54 -0700

    bug fixes

--- extraction/named_entity/biocppi_extraction/biocppi_extraction.py
+++ extraction/named_entity/biocppi_extraction/biocppi_extraction.py
@@ -301,7 +301,7 @@ class biocppi_extraction(Ner):
 
 
         # temp1.model_train(N,M,n_f,n_hidden,model,viterbi,trainer,lr,batch_size,wl,tl,save_checkpoint_models=False)
-        model = model_train(self.datapath,self.embeddings_path,self.optimizer,self.batch_size,
+        model = model_train(self.data_path_base,self.embeddings_path,self.optimizer,self.batch_size,
                     self.num_iterations,self.num_it_per_ckpt,self.learning_rate,self.embedding_factor,
                     self.decay_rate,self.keep_prob,self.num_cores,seed)  # train the model!
 
