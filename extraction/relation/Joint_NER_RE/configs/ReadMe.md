f81f3675efc43e55c72b79d15b663e83181d3b40
sanjithae <44079238+Sanjithae@users.noreply.github.com>
2019-05-01 04:49:44 -0700

    updates readme

---
+++ extraction/relation/Joint_NER_RE/configs/ReadMe.md
@@ -0,0 +1,1 @@
+You can place the new config files in this directory by creating a folder with the name of the dataset.
--- extraction/relation/Joint_NER_RE/main.py
+++ extraction/relation/Joint_NER_RE/main.py
@@ -1,7 +1,7 @@
 import multhead_joint_entity_relation_extraction
 from multhead_joint_entity_relation_extraction import multihead_joint_entity_relation_extraction
 obj=multihead_joint_entity_relation_extraction()
-filename="/home/sanjitha/Downloads/relation_extraction_test_input.txt"
+filename="data/relation_extraction_test_input.txt"
 data=obj.read_dataset(filename)
 
 obj.data_preprocess(data)
