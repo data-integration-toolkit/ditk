577e0e2c6196b6be6ef34b8f63d01a874d2f0784
trishav96 <trishav96@gmail.com>
2019-04-30 21:27:06 -0700

    Update main.py

--- graph/embedding/projE/main.py
+++ graph/embedding/projE/main.py
@@ -8,13 +8,13 @@ def main(input_file):
 
     args, trainList, validList, testList = model.read_dataset(input_file)
 
-    model.train_hrt_input, model.train_hrt_weight, model.train_trh_input, model.train_trh_weight, model.train_loss, model.train_op, model.ent_embeddings, model.rel_embeddings = model.learn_embeddings(data = args['data_dir'], argDict = args)
+    model.train_hrt_input, model.train_hrt_weight, file, model.train_trh_input, model.train_trh_weight, model.train_loss, model.train_op, model.ent_embeddings, model.rel_embeddings = model.learn_embeddings(data = args['data_dir'], argDict = args)
     model.test_input, model.test_head, model.test_tail = model.test_ops()
     load_dir = ""
     #load_dir = model.load_model("./trainFiles/ProjE_DEFAULT_0.ckpt")
 
     model.evaluate(data = args['data_dir'], args = args, load_dir = load_dir)
-    return model.ent_embeddings, model.rel_embeddings
+    return file
 
 if __name__ == '__main__':
     main('./yago_dataset/')
