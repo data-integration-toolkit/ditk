255db87719b5beadd5165293a6dce203fe77b78b
kendall <kcrouch@usc.edu>
2019-05-01 12:21:53 -0700

    bug fixes

--- extraction/named_entity/biocppi_extraction/biocppi_extraction.py
+++ extraction/named_entity/biocppi_extraction/biocppi_extraction.py
@@ -10,25 +10,9 @@ import biocppi_utils as butil
 from six.moves import cPickle
 import numpy as np
 
-from model import *
 # from utils import backtrack
 
 # from model1 import *
-
-sys.setrecursionlimit(5000)
-
-# ---------------------Helper for saving model-------------------- #
-import copy_reg
-import types
-import multiprocessing
-
-def _pickle_method(m):
-    if m.im_self is None:
-        return getattr, (m.im_class, m.im_func.func_name)
-    else:
-        return getattr, (m.im_self, m.im_func.func_name)
-
-copy_reg.pickle(types.MethodType, _pickle_method)
 
 # -------------------End Helper for saving model------------------ #
 
