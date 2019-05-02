c780f4d1066ad70ea1bf723b9e1c49b02c205229
kendall <kcrouch@usc.edu>
2019-05-01 17:06:49 -0700

    bug fixes

--- extraction/named_entity/biocppi_extraction/model.py
+++ extraction/named_entity/biocppi_extraction/model.py
@@ -211,7 +211,7 @@ class BiLSTM(object):
         self.train_step = self.opt.apply_gradients(grads_and_vars)        
         
         self.sess.run(tf.global_variables_initializer())
-        self.saver = tf.train.Saver(max_to_keep=10000)
+        self.saver = tf.train.Saver(max_to_keep=10000,save_relative_paths=True)
         # self.saver = tf.train.Saver()
     
     def _onehot(self, y, categories):
